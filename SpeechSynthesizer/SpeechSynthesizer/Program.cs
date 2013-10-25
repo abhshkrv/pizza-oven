80b0f78dd50bf6730c7c545c30b0cd49389368d0
Abhishek <abhishekravi1992@gmail.com>
2013-10-25 13:18:00 +0800

    final

--- SpeechSynthesizer/SpeechSynthesizer/Program.cs
+++ SpeechSynthesizer/SpeechSynthesizer/Program.cs
@@ -14,17 +14,18 @@ namespace SampleSynthesis
             using (SpeechSynthesizer synth = new SpeechSynthesizer())
             {
 
+                synth.Rate = -3;
                 // Configure the audio output.  Create the folder
-                synth.SetOutputToWaveFile(@"D:\temp\test.wav",
+                synth.SetOutputToWaveFile(@"D:\temp\test3.wav",
                   new SpeechAudioFormatInfo(8000, AudioBitsPerSample.Eight, AudioChannel.Mono));
 
                 // Create a SoundPlayer instance to play output audio file. 
                 System.Media.SoundPlayer m_SoundPlayer =
-                  new System.Media.SoundPlayer(@"D:\temp\test.wav");
+                  new System.Media.SoundPlayer(@"D:\temp\test3.wav");
 
                 // Build a prompt.
                 PromptBuilder builder = new PromptBuilder();
-                builder.AppendText("thousand");
+                builder.AppendText("15");
 
                 // Speak the prompt.
                 synth.Speak(builder);
